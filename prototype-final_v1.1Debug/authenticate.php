470b3d62b1ae898c1d1a78cc895ffbb8a9ef4c17
MichaelMooney2 <michaelmooney222@gmail.com>
2023-05-25 06:02:08 +1000

    Better dummy data, automated Unit Descriptions,  Applicant input their availability, Staff view applicants availability
    Better dummy data, automated Unit Descriptions,  Applicant input their availability, Staff view applicants availability - added functionality to generate Qualifications to profile page in a bit of a cursed manner - this still needs to be added in the front end

---
+++ prototype-final_v1.1Debug/authenticate.php
@@ -0,0 +1,57 @@
+<?php
+session_start();
+
+require 'connections.php';
+$username = $_REQUEST['username'];
+$password = $_REQUEST['password'];
+
+$sql_check = "SELECT * FROM login WHERE username='$username' and sPassword='$password' ";
+$result = $mysqli->query($sql_check);
+
+// Check if username and password pair exist
+if ($result->num_rows == 1) {
+    // Get row of user details
+    $sql_check = "SELECT * FROM systemUser WHERE username='$username'";
+    $result = $mysqli->query($sql_check);
+
+    $row = $result->fetch_assoc();
+    $userRole = $row['userRole'];
+    
+    if ($userRole=='staff') {
+        $sql_check = "SELECT * FROM login, systemUser WHERE login.username='$username' and systemUser.username='$username'";
+        $result = $mysqli->query($sql_check);
+        $row = $result->fetch_assoc();
+
+        $_SESSION['staffID'] = $row['userID'];
+        $_SESSION['userID'] = $row['userID'];
+        $_SESSION['givenName'] = $row['givenName'];
+
+    } else { //Default role is applicant
+
+        // Retrieve the row of data as an associative array
+        $sql_check = "SELECT * FROM login, systemUser WHERE login.username='$username' and systemUser.username='$username'";
+        $result = $mysqli->query($sql_check);
+        $row = $result->fetch_assoc();
+        $_SESSION['applicantID'] = $row['userID'];
+        $_SESSION['userID'] = $row['userID'];
+        $_SESSION['givenName'] = $row['givenName'];
+    }
+
+    // Set the session variables
+    $_SESSION['username'] = $username;
+    $_SESSION['userRole'] = $userRole;
+    $_SESSION['isLoggedIn'] = true;
+
+    // Redirect to the home page or some other page...
+    header('Location: index.php');
+    exit();
+
+} else { 
+    
+    $mysqli->close();
+    // username and password pair do not exist in database
+    header("Location: login.php?error=1");
+}
+
+
+?>
---
+++ prototype-final_v1.1Debug/units_staff_view.php
@@ -0,0 +1,108 @@
+<!-- HTML to show avaialble units, to view and edit preferences -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="style/unit.css">
+    <title>Units</title>
+    <?php
+        session_start();
+        require_once('utilities.php');
+        isLoggedIn();
+        isStaff();
+    ?>
+</head>
+<body>
+<?php
+    // Include the file containing the generateNav() function
+    include 'generateNav.php';
+
+    // Generate the navigation menu
+    $nav = generateNav();
+    echo $nav;
+?>
+<?php
+    require 'connections.php';
+    require_once('utilities.php');
+
+    // Get units available
+    $sql = "SELECT * FROM unit WHERE vacancyStatus='true' ";
+
+    $results = $mysqli->query($sql);
+    // Checks if results is empty
+    if (!$results) {
+        echo "Error: Our query failed to execute and here is why: " . PHP_EOL;
+        echo "Query: " . $sql . PHP_EOL;
+        echo "Errno: " . $mysqli->errno . PHP_EOL;
+        echo "Error: " . $mysqli->error . PHP_EOL;
+        exit;
+    } else { // Have results to show...
+        $row = 0;
+        $table = '';
+        
+        // Goes through each row from query results and append to TABLE
+        while ($data = $results->fetch_object()) {
+            // Get number of applicants applied to this unit
+            $unitCode = $data->unitCode;
+            $unitName = $data->unitName;
+
+            // Grab applicant preference for this unit
+            $sql = "SELECT DISTINCT userID, unitCode FROM preferences WHERE unitCode='$unitCode' ";
+            $result_object = $mysqli->query($sql);
+
+            // Get num of rows in result object
+            $n_applicants= $result_object->num_rows;
+
+            // Adding class info to each row of table
+            $table .= <<<TABLE
+            <tr class="classes">
+                <td>{$unitCode}</td>
+                <td><a href="unit_details.php?unitCode={$unitCode}">{$unitName}</a></td>
+                <td style='text-align: center;'><a href="class_sessions_staff_edit.php?unitCode={$unitCode}">Edit</a></td>
+                <td style='text-align: center;'><a href="">Remove</a></td>
+                <td style='text-align: right;'><a href="class_preferences_staff_view.php?unitCode={$unitCode}"><strong>  $n_applicants</strong></a></td>
+            </tr>
+TABLE;
+        };        
+    };
+        
+    // PHP dynamically generate table of available units 
+    echo <<<EOD
+    <div class="container my-5">
+        <div class="d-flex align-items-center align-self-center">
+            <div class="container-fluid d-flex justify-content-center">
+                <div class="card p-4 border-light">
+                    <h3 class="text-center mb-5">Available Units</h3>
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Id</th>
+                                <th>Name</th>
+                                <th>Edit</th>
+                                <th>Remove</th>
+                                <th>Applications</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {$table}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+EOD;
+    
+    // Clear resuls and close database
+    $results->free();
+    $mysqli->close();
+
+?>
+   
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>  
+</body>
+</html>
